725409b453ca62ff5d4d781ad1879779a5a18cfc
msaav3@gmail.com <msaav3@gmail.com>
2016-12-18 00:38:33 -0600

    added icon

--- public/auth.js
+++ public/auth.js
@@ -4,21 +4,26 @@ var client_request = "https://accounts.spotify.com/authorize";
 var client_redirect = "https://naaemjpfoncajeopegapflmhadcceiki.chromiumapp.org/?callback";
 var client_url = client_request + "/?client_id=" + client_id + "&response_type=code&redirect_uri=" + client_redirect + "&show_dialog=true";
 
-var token;
-
 function get_redirect_url () {
     return chrome.identity.getRedirectURL();
 }
 
-function launchAuthFlow () {
-  redirect_url = get_redirect_url() + "spotify";
+function launch_auth_flow () {
+  redirect_url = get_redirect_url();
   chrome.identity.launchWebAuthFlow(
     {'url': client_url, 'interactive': true},
-    function(redirect_url) {
+    function(responseUrl) {
         //Get access token
+        var regex = /=(.+)/; // match '=' and capture everything that follows
+        var token = responseUrl.match(regex);
+        token = token[1];
+        if (token) {
+           chrome.storage.local.set({'spotifyToken': token});
+        }
     });
 }
 
+
 window.onload = function(){
-  launchAuthFlow();
+  launch_auth_flow();
 }
